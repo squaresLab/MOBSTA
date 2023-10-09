0283ddd06c98ddd456580a4472ef3c7794bb5d34
ChrisTimperley <christimperley@googlemail.com>
2023-10-09 19:41:45 -0400

    simplified ros_install_deps.sh

--- deps/ros_install_deps.sh
+++ deps/ros_install_deps.sh
@@ -14,42 +14,31 @@
 # * NREC Internal Use (Use as Background IP to be cleared by CTTEC and the Project Manager/PI prior to use on another project).
 # * Created for Program: HPSTA - 55435.1.1990813
 # ****/
-(
-    set -e
-    set -x
 
-    # sudo apt install -y build-essential cmake
+set -eux
 
-    # # install ros 
-    # sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    # sudo apt install -y curl # if you haven't already installed curl
-    # curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-    # sudo apt update
-    # sudo apt install -y ros-noetic-ros-base ros-noetic-roscpp
+# install Gorgon
+pushd deps/gorgon-mutations-lib/
+cmake -DCMAKE_INSTALL_PREFIX=./lib -DGORGON_USE_OPENCV=OFF -B build
+cmake --build build
+sudo make -C build install
+popd
 
-    # install Gorgon
-    pushd deps/gorgon-mutations-lib/
-    cmake -DCMAKE_INSTALL_PREFIX=./lib -DGORGON_USE_OPENCV=OFF -B build
-    cmake --build build
-    sudo make -C build install
-    pushd
+# install Gorgon-ROS
+pushd deps/gorgon-ros/
+cmake -DCMAKE_INSTALL_PREFIX=./lib -DGORGON_USE_OPENCV=OFF -B build
+cmake --build build
+sudo make -C build install
+popd
 
-    # install Gorgon-Ros
-    pushd deps/gorgon-ros/
-    cmake -DCMAKE_INSTALL_PREFIX=./lib -DGORGON_USE_OPENCV=OFF -B build
-    cmake --build build
-    sudo make -C build install
-    pushd
+# install ros_msg_parser
+pushd deps/ros_msg_parser
+cmake -B build
+sudo make -C build install
+popd
 
-    # install ros_msg_parser
-    pushd deps/ros_msg_parser
-    cmake -B build
-    sudo make -C build install
-    pushd
-
-    # install ros_comm rosbag
-    pushd deps/ros_comm/tools/rosbag
-    cmake -B build
-    make -C build
-    pushd
-)
+# install ros_comm rosbag
+pushd deps/ros_comm/tools/rosbag
+cmake -B build
+make -C build
+popd
